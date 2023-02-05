c8d709849990167dcc3838f6a52f69df68a805b7
mazenkhattab97 <mazenkhattab97@hotmail.com>
2023-02-05 22:47:27 +0200

    intail commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { AdminpageComponent } from './adminpage/adminpage.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomepageComponent,
+    AdminpageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  baseurl = 'http://127.0.0.1:8000/api/'
+
+  constructor( private http :HttpClient) { }
+
+ public permission:any
+
+  public login (data:any){
+    return this.http.post(this.baseurl+'login',data)
+  }
+  public signnup (data:any){
+    return this.http.post(this.baseurl+'signup',data)
+  }
+
+ 
+
+
+ public CreateToken(token:string|any){
+    localStorage.setItem('authToken' , token );
+  }
+
+  public isloggedin(){
+    if(localStorage.getItem('authToken')){
+      return true
+    }else{
+      return false
+    }
+  }
+
+  public logout(){
+    localStorage.removeItem('authToken');
+    this.isloggedin()
+  }
+
+  
+}
